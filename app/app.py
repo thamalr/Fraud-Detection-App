94151a842fd97e4e6099558d1e9523c40b0e4815
Thamal R. Adikari <66732444+thamalr@users.noreply.github.com>
2025-08-05 19:14:50 +0530

    update app.py

--- app/app.py
+++ app/app.py
@@ -73,7 +73,7 @@ with tab1:
                     df = label_encode(df, catf)
                     df = frequency_encode(df, catf)
                     X = df.drop(columns=['isFraud'], errors='ignore')
-                    X = X.reindex(columns=xgb_model.get_booster().feature_names, fill_value=0)
+                    X = X.reindex(columns=xgb_model.feature_names, fill_value=0)
                     y_pred = xgb_model.predict(X)
                     df_output = pd.DataFrame({"TransactionID": tx_ids, "Prediction": y_pred})
 
